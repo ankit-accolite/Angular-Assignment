33a15c8de78ba542c1b183fd460231a5d72f813b
Ankit Garg <ankit.garg@accolitedigital.com>
2024-03-13 13:36:59 +0530

    Data Fetched and Displayed into the Components by *ngfor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { UserCardComponent } from '../user-card/user-card.component';
 import { Userdata } from '../userdata';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserCardComponent],
+  imports: [CommonModule,UserCardComponent,HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
     sample_user: Userdata ={
         id: 101,
         name: 'Test Name',
@@ -17,4 +18,22 @@ export class HomeComponent {
         city: 'India',
         phone: '9999999999'
     };
+
+      user_info: Userdata[] = [];
+
+    httpclient =inject(HttpClient);
+
+    ngOnInit(): void {
+      this.fetchData();
+    }
+
+    fetchData()
+    {
+      alert("Function Called");
+      this.httpclient.get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.user_info = data;
+      });
+    }
 }
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Userdata } from '../userdata';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
